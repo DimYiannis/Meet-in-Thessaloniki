e9275b0e63b2b1de4556e6b883c3de83bc67c907
yiannis <johndim99@gmail.com>
2023-03-09 16:46:29 +0200

    workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import {resolve} from "path";
 export default defineNuxtConfig({
+ 
   modules: ['@nuxtjs/tailwindcss','nuxt-swiper'],
 
   alias: {
